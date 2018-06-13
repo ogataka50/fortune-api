8ca33cd0d817c0a8e823b5f8e043cc611720166e
ogataka50 <ogataka50@gmail.com>
2018-06-13 19:00:51 +0900

    add new year test

--- fortune/drawing_test.go
+++ fortune/drawing_test.go
@@ -1,7 +1,7 @@
 package fortune
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -19,9 +19,29 @@ func TestHandler(t *testing.T) {
 	if rw.StatusCode != http.StatusOK {
 		t.Fatal("unexpected status code")
 	}
-	b, err := ioutil.ReadAll(rw.Body)
+	_, err := ioutil.ReadAll(rw.Body)
 	if err != nil {
 		t.Fatal("unexpected error")
 	}
-	fmt.Println(string(b))
+}
+
+func TestHandlerNewYear(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	h := Handler{Time: time.Date(2019, 1, 1, 0, 0, 0, 0, time.Local)}
+	h.ServeHTTP(w, r)
+	rw := w.Result()
+	defer rw.Body.Close()
+	if rw.StatusCode != http.StatusOK {
+		t.Fatal("unexpected status code")
+	}
+
+	var f fortune
+	if err := json.NewDecoder(rw.Body).Decode(&f); err != nil {
+		t.Fatal("unexpected error")
+	}
+
+	if f.Result != "大吉" {
+		t.Fatal("unexpected fortune...")
+	}
 }
